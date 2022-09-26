9bd40f14a919b2f37532baa6d77ed761f82efef0
Kvin <kevin.blenner@mindee.co>
2022-09-26 12:44:24 +0200

    test: cover invoice Mindee API call with some basics

---
+++ tests/Mindee.IntegrationTests/Api/InvoiceTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Mindee.Infrastructure.Api;
+using Mindee.Infrastructure.Api.Invoice;
+using Xunit;
+
+namespace Mindee.IntegrationTests.Api
+{
+    public class InvoiceTest
+    {
+        [Fact]
+        public async Task Predict_WithWrongApiKey_MustFail()
+        {
+            var api = new MindeeApi(
+                new NullLogger<MindeeApi>(),
+                Options.Create(
+                    new MindeeApiSettings() { ApiKey = "WrongKey" }));
+
+            await Assert.ThrowsAsync<HttpRequestException>(
+                () => api.PredictAsync(File.OpenRead("sample_2pages.pdf"), "sample_2pages.pdf"));
+        }
+
+        [Fact]
+        public async Task Predict_WithValidPdf_WichIsNotAnInvoice_MustSuccess()
+        {
+            var api = new MindeeApi(
+                new NullLogger<MindeeApi>(),
+                Options.Create(
+                    new MindeeApiSettings() { ApiKey = "GoodKey" }));
+
+            var invoicePredictResponse = await api.PredictAsync(File.OpenRead("sample_2pages.pdf"), "sample_2pages.pdf");
+
+            Assert.NotNull(invoicePredictResponse);
+        }
+
+        [Fact]
+        public async Task Predict_WithValidPdf_WichIsAnInvoice_MustSuccess()
+        {
+            var api = new MindeeApi(
+                new NullLogger<MindeeApi>(),
+                Options.Create(
+                    new MindeeApiSettings() { ApiKey = "GoodKey" }));
+
+            var invoicePredictResponse = await api.PredictAsync(File.OpenRead("inv2.pdf"), "inv2.pdf");
+
+            var expectedInvoiceResponse = JsonSerializer.Deserialize<InvoicePredictResponse>(File.ReadAllText("inv2.json"));
+
+            Assert.NotNull(invoicePredictResponse);
+            Assert.NotNull(expectedInvoiceResponse);
+            Assert.NotNull(invoicePredictResponse!.Document);
+            Assert.NotNull(expectedInvoiceResponse!.Document);
+            Assert.Equal(expectedInvoiceResponse.Document.Inference, expectedInvoiceResponse.Document.Inference);
+        }
+
+        [Fact]
+        public async Task Predict_WithPng_WichIsAnInvoice_MustSuccess()
+        {
+            var api = new MindeeApi(
+                new NullLogger<MindeeApi>(),
+                Options.Create(
+                    new MindeeApiSettings() { ApiKey = "GoodKey" }));
+
+            var invoicePredictResponse = await api.PredictAsync(File.OpenRead("inv1.png"), "inv1.png");
+
+            var expectedInvoiceResponse = JsonSerializer.Deserialize<InvoicePredictResponse>(File.ReadAllText("inv1.json"));
+
+            Assert.NotNull(invoicePredictResponse);
+            Assert.NotNull(expectedInvoiceResponse);
+            Assert.NotNull(invoicePredictResponse!.Document);
+            Assert.NotNull(expectedInvoiceResponse!.Document);
+            Assert.Equal(expectedInvoiceResponse.Document.Inference, expectedInvoiceResponse.Document.Inference);
+        }
+    }
+}
