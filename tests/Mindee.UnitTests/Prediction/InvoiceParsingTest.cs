729ade00a53b1e0f735cf518a6f3666608b6af2a
Kvin <kevin.blenner@mindee.co>
2022-09-27 10:15:40 +0200

    test: check if prediction values are correctly mapped

--- tests/Mindee.UnitTests/Prediction/InvoiceParsingTest.cs
+++ tests/Mindee.UnitTests/Prediction/InvoiceParsingTest.cs
@@ -12,22 +12,84 @@ namespace Mindee.UnitTests.Prediction
         [Fact]
         public async Task Execute_WithInvoicePdf_MustSuccess()
         {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.NotNull(invoicePrediction);
+        }
+
+        [Fact]
+        public async Task Execute_WithInvoicePdf_MustSuccessForCustomer()
+        {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.Equal(0.87, invoicePrediction.Customer.Confidence);
+            Assert.Equal(0, invoicePrediction.Customer.PageId);
+            Assert.Equal(new List<List<double>>()
+            {
+                new List<double>() { 0.072, 0.291 }, 
+                new List<double>() { 0.164, 0.291 }, 
+                new List<double>() { 0.164, 0.302 }, 
+                new List<double>() { 0.072, 0.302 },
+            }
+            , invoicePrediction.Customer.Polygon);
+            Assert.Equal("TEST BUSINESS", invoicePrediction.Customer.Value);
+        }
+
+        [Fact]
+        public async Task Execute_WithInvoicePdf_MustSuccessForDate()
+        {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.Equal(0.99, invoicePrediction.Date.Confidence);
+            Assert.Equal(0, invoicePrediction.Date.PageId);
+            Assert.Equal("2016-01-25", invoicePrediction.Date.Value);
+        }
+
+        [Fact]
+        public async Task Execute_WithInvoicePdf_MustSuccessForDocumentType()
+        {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.Equal("INVOICE", invoicePrediction.DocumentType.Value);
+        }
+
+        [Fact]
+        public async Task Execute_WithInvoicePdf_MustSuccessForLocale()
+        {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.Equal("en", invoicePrediction.Locale.Language);
+            Assert.Equal("USD", invoicePrediction.Locale.Currency);
+        }
+
+        [Fact]
+        public async Task Execute_WithInvoicePdf_MustSuccessForTotalTaxesIncluded()
+        {
+            IInvoiceParsing invoiceParsing = new InvoiceParsing(GetMindeeApi());
+            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
+
+            Assert.Equal(93.5, invoicePrediction.TotalIncl.Value);
+        }
+
+        private MindeeApi GetMindeeApi()
+        {
             var mockHttp = new MockHttpMessageHandler();
             mockHttp.When("*")
                     .Respond("application/json", File.ReadAllText("inv2.json"));
-            var mindeeApi = new MindeeApi(
-                new NullLogger<MindeeApi>(), 
+
+            return new MindeeApi(
+                new NullLogger<MindeeApi>(),
                 Options.Create(new MindeeApiSettings()
                 {
                     ApiKey = "Expelliarmus"
                 }),
                 mockHttp
                 );
-
-            IInvoiceParsing invoiceParsing = new InvoiceParsing(mindeeApi);
-            var invoicePrediction = await invoiceParsing.ExecuteAsync(Stream.Null, "Bou");
-
-            Assert.NotNull(invoicePrediction);
         }
     }
 }
