0aad9b5710d0b365fb45349df739afd6edfe86de
Kvin <kevin.blenner@mindee.co>
2022-10-04 16:29:17 +0200

    refactor: deleting useless inference object by OTS features

--- src/Mindee.Domain/Parsing/Receipt/ReceiptInference.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mindee.Domain.Parsing.Common;
-
-namespace Mindee.Domain.Parsing.Receipt
-{
-    public sealed class ReceiptInference
-    {
-        public Inference<ReceiptPrediction> Inference { get; set; }
-    }
-}
