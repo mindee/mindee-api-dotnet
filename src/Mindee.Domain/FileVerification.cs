ee198fe93798bdbe3ccdaf1eeb600947aa61cc4a
Kvin <kevin.blenner@mindee.co>
2022-10-04 17:34:51 +0200

    feat: adding file verification with extension check

---
+++ src/Mindee.Domain/FileVerification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Mindee.Domain
+{
+    static public class FileVerification
+    {
+        private static readonly string[] _authorizedFileExtensions =
+            { 
+                ".pdf",
+                ".webp",
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".heic",
+                ".tiff",
+                ".tif",
+            };
+
+        public static bool IsFileNameExtensionRespectLimitation(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+
+            return _authorizedFileExtensions.Any(f => f.Equals(extension, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
