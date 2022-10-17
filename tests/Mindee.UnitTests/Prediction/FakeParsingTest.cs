c50bf362a540abcbb13b35681bebc6cddc0719d4
Kvin <kevin.blenner@mindee.co>
2022-10-17 09:26:22 +0200

    test: a prediction model without endpoint attribute must fail

---
+++ tests/Mindee.UnitTests/Prediction/FakeParsingTest.cs
@@ -0,0 +1,53 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Mindee.Parsing;
+using Mindee.Parsing.Common;
+using RichardSzalay.MockHttp;
+
+namespace Mindee.UnitTests.Prediction
+{
+    public class FakeParsingTest
+    {
+        [Fact]
+        public async Task Execute_WithAnyKindOfData_MustFail()
+        {
+            var mindeeAPi = GetMindeeApi();
+
+            await Assert.ThrowsAsync<NotSupportedException>(
+               () => _ = mindeeAPi.PredictAsync<FakePrediction>(GetFakePredictParameter()));
+        }
+
+        private PredictParameter GetFakePredictParameter()
+        {
+            return
+                new PredictParameter(
+                    new byte[] { byte.MinValue },
+                        "Bou");
+        }
+
+        private static MindeeApi GetMindeeApi(string fileName = "Resources/receipt_response_full_v3.json")
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When("*")
+                    .Respond("application/json", File.ReadAllText(fileName));
+
+            var config = new ConfigurationBuilder()
+                            .AddInMemoryCollection(new Dictionary<string, string>() {
+                                { "MindeeApiSettings:ApiKey", "blou" }
+                            })
+                        .Build();
+
+            return new MindeeApi(
+                new NullLogger<MindeeApi>(),
+                config,
+                mockHttp
+                );
+        }
+    }
+
+    internal sealed class FakePrediction : PredictionBase
+    {
+
+    }
+}
