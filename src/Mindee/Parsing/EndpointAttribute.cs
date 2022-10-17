062b24630588a71b9dd13e904b0cda069be17b69
Kvin <kevin.blenner@mindee.co>
2022-10-17 11:22:04 +0200

    docs: improve endpoint attribute

--- src/Mindee/Parsing/EndpointAttribute.cs
+++ src/Mindee/Parsing/EndpointAttribute.cs
@@ -8,6 +8,11 @@ namespace Mindee.Parsing
         private readonly string _productName;
         private readonly string _version;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productName">The name of the product associated to the expected model</param>
+        /// <param name="version">The version number of the API. Without the v (for example for the v1.2: 1.2)</param>
         public EndpointAttribute(string productName, string version)
         {
             _productName = productName;
