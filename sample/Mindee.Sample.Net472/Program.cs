c878e7e5204dfdc442e9c52d18daaeb35a026c00
Kvin <kevin.blenner@mindee.co>
2022-11-28 18:29:20 +0100

    ci

--- sample/Mindee.Sample.Net472/Program.cs
+++ sample/Mindee.Sample.Net472/Program.cs
@@ -3,24 +3,22 @@ using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
+using Mindee;
 using Mindee.Parsing.Receipt;
 
-namespace Mindee.Sample.Net472
+internal static class Program
 {
-    internal static class Program
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
-        {
-            string filePath = @"C:\Users\MyUser\Downloads\myReceipt.pdf";
+        string filePath = @"C:\Users\MyUser\Downloads\myReceipt.pdf";
 
-            var mindeeClient = new MindeeClient(
-                Options.Create(new MindeeSettings() { ApiKey = "MyKey" }));
+        var mindeeClient = new MindeeClient(
+            Options.Create(new MindeeSettings() { ApiKey = "MyKey" }));
 
-            var receiptV4Prediction = await mindeeClient.LoadDocument(new FileInfo(filePath))
-                .ParseAsync<ReceiptV4Prediction>();
+        var receiptV4Prediction = await mindeeClient.LoadDocument(new FileInfo(filePath))
+            .ParseAsync<ReceiptV4Prediction>();
 
-            Console.WriteLine(JsonSerializer.Serialize(receiptV4Prediction, new JsonSerializerOptions { WriteIndented = true }));
-            Console.ReadKey();
-        }
+        Console.WriteLine(JsonSerializer.Serialize(receiptV4Prediction, new JsonSerializerOptions { WriteIndented = true }));
+        Console.ReadKey();
     }
 }
--- sample/Mindee.Sample.Net60/Program.cs
+++ sample/Mindee.Sample.Net60/Program.cs
@@ -1,23 +1,21 @@
 ﻿using System.Text.Json;
 using Microsoft.Extensions.Options;
+using Mindee;
 using Mindee.Parsing.Receipt;
 
-namespace Mindee.Sample.Net60
+internal static class Program
 {
-    internal static class Program
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
-        {
-            string filePath = @"C:\Users\33621\Downloads\t2i.pdf";
+        string filePath = @"C:\Users\33621\Downloads\t2i.pdf";
 
-            var mindeeClient = new MindeeClient(
-                Options.Create(new MindeeSettings() { ApiKey = "MyKey" }));
+        var mindeeClient = new MindeeClient(
+            Options.Create(new MindeeSettings() { ApiKey = "MyKey" }));
 
-            var receiptV4Prediction = await mindeeClient.LoadDocument(new FileInfo(filePath))
-                .ParseAsync<ReceiptV4Prediction>();
+        var receiptV4Prediction = await mindeeClient.LoadDocument(new FileInfo(filePath))
+            .ParseAsync<ReceiptV4Prediction>();
 
-            Console.Write(JsonSerializer.Serialize(receiptV4Prediction, new JsonSerializerOptions { WriteIndented = true }));
-            Console.ReadKey();
-        }
+        Console.Write(JsonSerializer.Serialize(receiptV4Prediction, new JsonSerializerOptions { WriteIndented = true }));
+        Console.ReadKey();
     }
 }
