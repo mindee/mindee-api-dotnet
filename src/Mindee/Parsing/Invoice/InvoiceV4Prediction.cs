70f2a1d4b62adb9b7d305f521dddc7c1d5a44663
Kvin <kevin.blenner@mindee.co>
2022-12-01 16:56:50 +0100

    feat: support Invoice v4 and line items

---
+++ src/Mindee/Parsing/Invoice/InvoiceV4Prediction.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Mindee.Parsing.Common;
+
+namespace Mindee.Parsing.Invoice
+{
+    /// <summary>
+    /// The invoice model for the v4.
+    /// </summary>
+    [Endpoint("invoices_beta", "4")]
+    public sealed class InvoiceV4Prediction : FinancialPredictionBase
+    {
+        /// <summary>
+        /// The supplier name.
+        /// </summary>
+        [JsonPropertyName("supplier_name")]
+        public StringField SupplierName { get; set; }
+
+        /// <summary>
+        /// List of <see cref="CompanyRegistration"/>
+        /// </summary>
+        [JsonPropertyName("supplier_company_registrations")]
+        public List<CompanyRegistration> SupplierCompanyRegistrations { get; set; }
+
+        /// <summary>
+        /// List of payment details.
+        /// </summary>
+        [JsonPropertyName("supplier_payment_details")]
+        public List<PaymentDetail> SupplierPaymentDetails { get; set; }
+
+        /// <summary>
+        /// The supplier address.
+        /// </summary>
+        [JsonPropertyName("supplier_address")]
+        public StringField SupplierAddress { get; set; }
+
+        /// <summary>
+        /// The customer.
+        /// </summary>
+        [JsonPropertyName("customer_name")]
+        public StringField CustomerName { get; set; }
+
+        /// <summary>
+        /// List of customer company registrations.
+        /// </summary>
+        [JsonPropertyName("customer_company_registrations")]
+        public List<CustomerCompanyRegistration> CustomerCompanyRegistrations { get; set; }
+
+        /// <summary>
+        /// The adress of the customer.
+        /// </summary>
+        [JsonPropertyName("customer_address")]
+        public StringField CustomerAddress { get; set; }
+
+        /// <summary>
+        /// The due date of the invoice.
+        /// </summary>
+        [JsonPropertyName("due_date")]
+        public Date DueDate { get; set; }
+
+        /// <summary>
+        /// The invoice number.
+        /// </summary>
+        [JsonPropertyName("invoice_number")]
+        public StringField InvoiceNumber { get; set; }
+
+        /// <summary>
+        /// Total amount including taxes.
+        /// </summary>
+        [JsonPropertyName("total_amount")]
+        public AmountField TotalAmount { get; set; }
+
+        /// <summary>
+        /// Total amount excluding taxes.
+        /// </summary>
+        [JsonPropertyName("total_net")]
+        public AmountField TotalNet { get; set; }
+
+        /// <summary>
+        /// Line items details.
+        /// </summary>
+        [JsonPropertyName("line_items")]
+        public List<InvoiceLineItem> LineItems { get; set; }
+
+        /// <summary>
+        /// A prettier reprensentation of the current model values.
+        /// </summary>
+        public override string ToString()
+        {
+            string lineItems = "\n";
+            if (LineItems.Any())
+            {
+                lineItems =
+                    "\n  Code           | QTY    | Price   | Amount   | Tax (Rate)     | Description\n  ";
+                lineItems += string.Join("\n  ", LineItems.Select(item => item.ToString()));
+            }
+
+            StringBuilder result = new StringBuilder("----- Invoice v4 -----\n");
+            result.Append($"Invoice number: {InvoiceNumber.Value}\n");
+            result.Append($"Locale: {Locale}\n");
+            result.Append($"Invoice date: {Date.Value}\n");
+            result.Append($"Invoice due date: {DueDate.Value}\n");
+            result.Append($"Supplier name: {SupplierName.Value}\n");
+            result.Append($"Supplier address: {SupplierAddress.Value}\n");
+            result.Append($"Supplier company registrations: {string.Join("\n                 ", SupplierCompanyRegistrations.Select(c => c.Value))}\n");
+            result.Append($"Supplier payment details: {string.Join("\n                 ", SupplierPaymentDetails.Select(p => p))}\n");
+            result.Append($"Customer name: {CustomerName.Value}\n");
+            result.Append($"Customer company registrations: {string.Join("; ", CustomerCompanyRegistrations.Select(c => c.Value))}\n");
+            result.Append($"Customer address: {string.Join("; ", CustomerAddress.Value)}\n");
+            result.Append($"Taxes: {string.Join("\n                 ", Taxes.Select(t => t))}\n");
+            result.Append($"Line items: {lineItems}\n");
+            result.Append($"Total amount including taxes: {TotalAmount.Value}\n");
+            result.Append($"Total amount excluding taxes: {TotalNet.Value}\n");
+            result.Append("----------------------");
+
+            return result.ToString();
+        }
+    }
+}
---
+++ tests/Mindee.UnitTests/Parsing/Invoice/InvoiceV4Test.cs
@@ -0,0 +1,212 @@
+﻿using Mindee.Parsing;
+using Mindee.Parsing.Invoice;
+
+namespace Mindee.UnitTests.Parsing.Invoice
+{
+    public class InvoiceV4Test
+    {
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccess()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.NotNull(invoicePrediction);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForInvoiceNumber()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("0042004801351",
+                invoicePrediction.Inference.Pages.First().Prediction.InvoiceNumber.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForCustomerName()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal(0, invoicePrediction.Inference.Pages.First().Prediction.CustomerName.Confidence);
+            Assert.Equal(0, invoicePrediction.Inference.Pages.First().Id);
+            Assert.Equal(new List<List<double>>()
+            , invoicePrediction.Inference.Pages.First().Prediction.CustomerName.Polygon);
+            Assert.Null(invoicePrediction.Inference.Pages.First().Prediction.CustomerName.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForCustomerCompanyRegistrations()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("FR00000000000",
+                invoicePrediction.Inference.Prediction.CustomerCompanyRegistrations.First().Value);
+            Assert.Equal("111222333",
+                invoicePrediction.Inference.Prediction.CustomerCompanyRegistrations.Last().Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForCustomerAddress()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("1954 Bloon Street West Toronto, ON, M6P 3K9 Canada",
+                invoicePrediction.Inference.Pages.Last().Prediction.CustomerAddress.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForDate()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal(0.99, invoicePrediction.Inference.Pages.First().Prediction.Date.Confidence);
+            Assert.Equal(0, invoicePrediction.Inference.Pages.First().Id);
+            Assert.Equal("2020-02-17", invoicePrediction.Inference.Pages.First().Prediction.Date.Value);
+            Assert.Equal(new List<List<double>>()
+            {
+                new List<double>() { 0.382, 0.306 },
+                new List<double>() { 0.44, 0.306 },
+                new List<double>() { 0.44, 0.318 },
+                new List<double>() { 0.382, 0.318 },
+            }
+            , invoicePrediction.Inference.Pages.First().Prediction.Date.Polygon);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForDueDate()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("2020-02-17", invoicePrediction.Inference.Pages.First().Prediction.DueDate.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForDocumentType()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("INVOICE", invoicePrediction.Inference.Pages.First().Prediction.DocumentType.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForLocale()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("fr", invoicePrediction.Inference.Pages.First().Prediction.Locale.Language);
+            Assert.Equal("EUR", invoicePrediction.Inference.Pages.First().Prediction.Locale.Currency);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForTotalTaxesIncluded()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal(587.95, invoicePrediction.Inference.Pages.First().Prediction.TotalAmount.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForTotalTaxesExcluded()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal(489.97, invoicePrediction.Inference.Pages.First().Prediction.TotalNet.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForSupplierName()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("TURNPIKE DESIGNS CO.", invoicePrediction.Inference.Pages.Last().Prediction.SupplierName.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForSupplierAddress()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("156 University Ave, Toronto ON, Canada M5H 2H7",
+                invoicePrediction.Inference.Pages.Last().Prediction.SupplierAddress.Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForSupplierCompanyRegistrations()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("501124705", invoicePrediction.Inference.Pages.First().Prediction.SupplierCompanyRegistrations.First().Value);
+            Assert.Equal("FR33501124705", invoicePrediction.Inference.Pages.First().Prediction.SupplierCompanyRegistrations.Last().Value);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForSupplierPaymentDetails()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal("FR7640254025476501124705368", invoicePrediction.Inference.Pages.First().Prediction.SupplierPaymentDetails.First().Iban);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForLineItems()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.NotEmpty(invoicePrediction.Inference.Prediction.LineItems);
+
+            Assert.Equal("XXX81125600010", invoicePrediction.Inference.Prediction.LineItems.Skip(2).First().ProductCode);
+            Assert.Equal(1.0, invoicePrediction.Inference.Prediction.LineItems.Skip(2).First().Quantity);
+            Assert.Equal("a long string describing the item",
+                invoicePrediction.Inference.Prediction.LineItems.Skip(2).First().Description);
+            Assert.Equal(4.31, invoicePrediction.Inference.Prediction.LineItems.First().TotalAmount);
+            Assert.Equal(2.1, invoicePrediction.Inference.Prediction.LineItems.First().TaxRate);
+        }
+
+        [Fact]
+        [Trait("Category", "Invoice V4")]
+        public async Task Predict_MustSuccessForOrientation()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var invoicePrediction = await mindeeAPi.PredictAsync<InvoiceV4Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            Assert.Equal(0, invoicePrediction.Inference.Pages.First().Orientation.Value);
+        }
+
+        private MindeeApi GetMindeeApiForInvoice(string fileName = "Resources/invoice/response_v4/complete.json")
+        {
+            return ParsingTestBase.GetMindeeApi(fileName);
+        }
+    }
+}
