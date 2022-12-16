62ba19fdefb525c262a17fe4a89b6090199e32c7
Kvin <kevin.blenner@mindee.co>
2022-12-16 15:04:17 +0100

    feat: Support Shipping Container V1

---
+++ src/Mindee/Parsing/ShippingContainer/ShippingContainerV1Prediction.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using Mindee.Parsing.Common;
+
+namespace Mindee.Parsing.ShippingContainer
+{
+    /// <summary>
+    /// The shipping container model for the v1.
+    /// </summary>
+    [Endpoint("shipping_containers", "1")]
+    public sealed class ShippingContainerV1Prediction : PredictionBase
+    {
+        /// <summary>
+        /// ISO 6346 code for container owner prefix + equipment identifier.
+        /// </summary>
+        [JsonPropertyName("owner")]
+        public StringField Owner { get; set; }
+
+        /// <summary>
+        /// ISO 6346 code for container serial number (6+1 digits).
+        /// </summary>
+        [JsonPropertyName("serial_number")]
+        public StringField SerialNumber { get; set; }
+
+        /// <summary>
+        /// ISO 6346 code for container length, height and type.
+        /// </summary>
+        [JsonPropertyName("size_type")]
+        public StringField SizeType { get; set; }
+
+        /// <summary>
+        /// A prettier reprensentation of the current model values.
+        /// </summary>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("----- Shipping Container V1 -----\n");
+            result.Append($"Owner: {Owner.Value}\n");
+            result.Append($"Serial number: {SerialNumber.Value}\n");
+            result.Append($"Size and type: {SizeType.Value}\n");
+
+            result.Append("----------------------\n");
+
+            return SummaryHelper.Clean(result.ToString());
+        }
+    }
+}
---
+++ tests/Mindee.UnitTests/Parsing/ShippingContainer/ShippingContainerV1Test.cs
@@ -0,0 +1,36 @@
+﻿using Mindee.Parsing;
+using Mindee.Parsing.ShippingContainer;
+
+namespace Mindee.UnitTests.Parsing.ShippingContainer
+{
+    [Trait("Category", "Shipping Container")]
+    public class ShippingContainerV1Test
+    {
+        [Fact]
+        public async Task Predict_CheckSummary()
+        {
+            var mindeeAPi = GetMindeeApiForCarteVitale();
+            var prediction = await mindeeAPi.PredictAsync<ShippingContainerV1Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            var expected = File.ReadAllText("Resources/shipping_container/response_v1/doc_to_string.txt");
+
+            Assert.Equal(
+                CleaningResult(expected),
+                prediction.Inference.Prediction.ToString());
+        }
+
+        private string CleaningResult(string expectedSummary)
+        {
+            var indexFilename = expectedSummary.IndexOf("Filename");
+            var indexFilenameEOL = expectedSummary.IndexOf("\n", indexFilename);
+            string cleanedSummary = expectedSummary.Remove(indexFilename, indexFilenameEOL - indexFilename + 1);
+
+            return cleanedSummary;
+        }
+
+        private MindeeApi GetMindeeApiForCarteVitale(string fileName = "Resources/shipping_container/response_v1/complete.json")
+        {
+            return ParsingTestBase.GetMindeeApi(fileName);
+        }
+    }
+}
