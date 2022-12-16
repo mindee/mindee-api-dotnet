5b7363838f8251e0b6013e5a863e106bc68b7310
Kvin <kevin.blenner@mindee.co>
2022-12-16 15:26:56 +0100

    feat: Support EU License plates V1

---
+++ src/Mindee/Parsing/Eu/LicensePlatesV1Prediction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Mindee.Parsing.Common;
+
+namespace Mindee.Parsing.LicensePlates
+{
+    /// <summary>
+    /// The eu licence plates model for the v1.
+    /// </summary>
+    [Endpoint("license_plates", "1")]
+    public sealed class LicensePlatesV1Prediction : PredictionBase
+    {
+        /// <summary>
+        /// A list of license plates values.
+        /// </summary>
+        [JsonPropertyName("license_plates")]
+        public IList<StringField> LicensePlates { get; set; } = new List<StringField>();
+
+        /// <summary>
+        /// A prettier reprensentation of the current model values.
+        /// </summary>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("----- EU License plate V1 -----\n");
+            result.Append($"License plates: {string.Join(", ", LicensePlates.Select(lp => lp.Value))}\n");
+
+            result.Append("----------------------\n");
+
+            return SummaryHelper.Clean(result.ToString());
+        }
+    }
+}
---
+++ tests/Mindee.UnitTests/Parsing/Eu/LicensePlatesV1Test.cs
@@ -0,0 +1,36 @@
+﻿using Mindee.Parsing;
+using Mindee.Parsing.LicensePlates;
+
+namespace Mindee.UnitTests.Parsing.Eu.LicensePlates
+{
+    [Trait("Category", "EU License plates V1")]
+    public class LicensePlatesV1Test
+    {
+        [Fact]
+        public async Task Predict_CheckSummary()
+        {
+            var mindeeAPi = GetMindeeApiForLicensePlates();
+            var prediction = await mindeeAPi.PredictAsync<LicensePlatesV1Prediction>(ParsingTestBase.GetFakePredictParameter());
+
+            var expected = File.ReadAllText("Resources/eu/license_plate/response_v1/doc_to_string.txt");
+
+            Assert.Equal(
+                CleaningResult(expected),
+                prediction.Inference.Prediction.ToString());
+        }
+
+        private string CleaningResult(string expectedSummary)
+        {
+            var indexFilename = expectedSummary.IndexOf("Filename");
+            var indexFilenameEOL = expectedSummary.IndexOf("\n", indexFilename);
+            string cleanedSummary = expectedSummary.Remove(indexFilename, indexFilenameEOL - indexFilename + 1);
+
+            return cleanedSummary;
+        }
+
+        private MindeeApi GetMindeeApiForLicensePlates(string fileName = "Resources/eu/license_plate/response_v1/complete.json")
+        {
+            return ParsingTestBase.GetMindeeApi(fileName);
+        }
+    }
+}
