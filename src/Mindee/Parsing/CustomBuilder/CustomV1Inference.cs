0a988532be7bf24e5f1d6905553a2dcaf90fd192
Kvin <kevin.blenner@mindee.co>
2023-01-20 16:02:01 +0100

    :sparkles: support classification type for custom document

--- src/Mindee/Parsing/CustomBuilder/CustomV1Inference.cs
+++ src/Mindee/Parsing/CustomBuilder/CustomV1Inference.cs
@@ -1,12 +1,11 @@
 ﻿using Mindee.Parsing.Common;
-using Mindee.Parsing.CustomBuilder;
 
 namespace Mindee.Parsing.CustomBuilder
 {
     /// <summary>
     /// The custom document v1 definition.
     /// </summary>
-    public class CustomV1Inference : Inference<CustomPrediction, CustomPrediction>
+    public class CustomV1Inference : Inference<CustomV1PagePrediction, CustomV1DocumentPrediction>
     {
     }
 }
---
+++ src/Mindee/Parsing/CustomBuilder/CustomV1PagePrediction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using Mindee.Parsing.Common;
+
+namespace Mindee.Parsing.CustomBuilder
+{
+    /// <summary>
+    /// Define a page prediction from a model built thanks to an API builder.
+    /// </summary>
+    [Serializable]
+    public sealed class CustomV1PagePrediction : Dictionary<string, ListField>
+    {
+        /// <summary>
+        /// Default empty constructor.
+        /// </summary>
+        public CustomV1PagePrediction()
+        {
+        }
+
+        private CustomV1PagePrediction(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        /// <summary>
+        /// A prettier reprensentation.
+        /// </summary>
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var listField in this)
+            {
+                result.Append($":{listField.Key}: {listField.Value}\n");
+            }
+
+            return SummaryHelper.Clean(result.ToString());
+        }
+    }
+}
