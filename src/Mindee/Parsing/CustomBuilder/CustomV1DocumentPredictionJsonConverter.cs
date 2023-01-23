14ddb6503e189b50b5122591547f831afe1b5ace
Kvin <kevin.blenner@mindee.co>
2023-01-23 09:30:41 +0100

    :rotating_light:

--- src/Mindee/Parsing/CustomBuilder/CustomV1DocumentPredictionJsonConverter.cs
+++ src/Mindee/Parsing/CustomBuilder/CustomV1DocumentPredictionJsonConverter.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Text.Json.Serialization;
-using System.Text.Json;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Mindee.Parsing.CustomBuilder
 {
