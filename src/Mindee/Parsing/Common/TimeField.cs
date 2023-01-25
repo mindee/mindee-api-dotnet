aee1392f2b7ecaf16db99ac6248269dfbaed41df
Kvin <kevin.blenner@mindee.co>
2023-01-25 12:05:12 +0100

    :boom: renaming Time to TimeField

--- src/Mindee/Parsing/Common/TimeField.cs
+++ src/Mindee/Parsing/Common/TimeField.cs
@@ -1,19 +1,13 @@
 ﻿using System.Text.Json.Serialization;
 using Mindee.Parsing.Common;
 
-namespace Mindee.Parsing.Receipt
+namespace Mindee.Parsing.Common
 {
     /// <summary>
     /// Represent a time.
     /// </summary>
-    public class Time : BaseField
+    public class TimeField : BaseField
     {
-        /// <summary>
-        /// The raw representation of the value
-        /// </summary>
-        [JsonPropertyName("raw")]
-        public string Raw { get; set; }
-
         /// <summary>
         /// The value of the field.
         /// </summary>
