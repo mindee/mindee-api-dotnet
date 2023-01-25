178c88de005d2f7318915592620b076f0b4d3c17
Kvin <kevin.blenner@mindee.co>
2023-01-25 11:48:53 +0100

    :sparkles: support Proof of Address V1

---
+++ tests/Mindee.UnitTests/Parsing/ProofOfAddress/ProofOfAddressV1Test.cs
@@ -0,0 +1,27 @@
+﻿using Mindee.Parsing;
+using Mindee.Parsing.ProofOfAddress;
+
+namespace Mindee.UnitTests.Parsing.ProofOfAddress
+{
+    [Trait("Category", "Proof of Address V1")]
+    public class ProofOfAddressV1Test
+    {
+        [Fact]
+        public async Task Predict_CheckSummary()
+        {
+            var mindeeAPi = GetMindeeApiForInvoice();
+            var prediction = await mindeeAPi.PredictAsync<ProofOfAddressV1Inference>(ParsingTestBase.GetFakePredictParameter());
+
+            var expected = File.ReadAllText("Resources/proof_of_address/response_v1/summary_full.rst");
+
+            Assert.Equal(
+                expected,
+                prediction.ToString());
+        }
+
+        private MindeeApi GetMindeeApiForInvoice(string fileName = "Resources/proof_of_address/response_v1/complete.json")
+        {
+            return ParsingTestBase.GetMindeeApi(fileName);
+        }
+    }
+}
