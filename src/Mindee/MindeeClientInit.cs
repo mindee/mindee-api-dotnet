117d384500e236d88ed16b6ac5f6d6b8f39ebd6d
Kvin <kevin.blenner@mindee.co>
2023-01-30 11:06:48 +0100

    :sparkles: expose a static helper to instantiate a new MindeeClient

---
+++ src/Mindee/MindeeClientInit.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Options;
+using Mindee.Pdf;
+
+namespace Mindee
+{
+    /// <summary>
+    /// Helper to create manually new instance of <see cref="MindeeClient"/>.
+    /// </summary>
+    public static class MindeeClientInit
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="apiKey">The required API key to use Mindee.</param>
+        public static MindeeClient Create(string apiKey)
+        {
+            var mindeeSettings = new MindeeSettings
+            {
+                ApiKey = apiKey
+            };
+
+            return Create(mindeeSettings);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="mindeeSettings"><see cref="MindeeSettings"/></param>
+        public static MindeeClient Create(MindeeSettings mindeeSettings)
+        {
+            return new MindeeClient(
+                Options.Create(mindeeSettings),
+                new DocNetApi());
+        }
+    }
+}
