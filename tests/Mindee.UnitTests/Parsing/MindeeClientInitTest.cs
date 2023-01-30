f22f52d0b454b201f8febc8a809c121b9888a41e
Kvin <kevin.blenner@mindee.co>
2023-01-30 11:15:24 +0100

    :white_check_mark: add mindee api test and mindee client init tests

--- tests/Mindee.UnitTests/Parsing/MindeeClientInitTest.cs
+++ tests/Mindee.UnitTests/Parsing/MindeeClientInitTest.cs
@@ -1,16 +1,15 @@
 ﻿namespace Mindee.UnitTests.Parsing
 {
+    [Trait("Category", "MindeeClient init")]
     public class MindeeClientInitTest
     {
         [Fact]
-        [Trait("Category", "MindeeClient init")]
         public async Task Create_WithApiKey()
         {
             Assert.NotNull(MindeeClientInit.Create("MyApiKey"));
         }
 
         [Fact]
-        [Trait("Category", "MindeeClient init")]
         public async Task Create_WithoutApiKey()
         {
             Assert.NotNull(MindeeClientInit.Create(new MindeeSettings() { ApiKey = "MyApiKey" }));
