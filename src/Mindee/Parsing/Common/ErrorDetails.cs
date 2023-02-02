19876db35d4dbe704931a662b31de497ede4b420
Kvin <kevin.blenner@mindee.co>
2023-02-02 11:25:49 +0100

    :bug:! Details property in Error become an object
    because it can handle both string and object

---
+++ src/Mindee/Parsing/Common/ErrorDetails.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Mindee.Parsing.Common
+{
+    /// <summary>
+    /// Represent an error information from the API response.
+    /// </summary>
+
+    [JsonConverter(typeof(ErrorDetailsJsonConverter))]
+    public class ErrorDetails
+    {
+        /// <summary>
+        /// A code to identify it.
+        /// </summary>
+        public string Value { get; }
+
+        public ErrorDetails(string value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// To make the error prettier to display.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
---
+++ src/Mindee/Parsing/Common/ErrorDetailsJsonConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Mindee.Parsing.Common
+{
+    /// <summary>
+    /// Custom de-serialize for <see cref="ErrorDetails"/>
+    /// </summary>
+    public class ErrorDetailsJsonConverter : JsonConverter<ErrorDetails>
+    {
+        /// <summary>
+        /// <see cref="Read(ref Utf8JsonReader, Type, JsonSerializerOptions)"/>
+        /// </summary>
+        public override ErrorDetails Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string errorDetails;
+
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                JsonObject jsonObject = (JsonObject)JsonSerializer.Deserialize(
+                    ref reader,
+                    typeof(JsonObject),
+                    options);
+
+                errorDetails = jsonObject.ToJsonString();
+            }
+            else if (reader.TokenType == JsonTokenType.String)
+            {
+                errorDetails = reader.GetString();
+            }
+            else
+            {
+                throw new InvalidOperationException("The JSON type is not handled.");
+            }
+
+            return new ErrorDetails(errorDetails);
+        }
+
+        /// <summary>
+        /// <summary>
+        /// <see cref="Write(Utf8JsonWriter, ErrorDetails, JsonSerializerOptions)"/>
+        /// </summary>
+        public override void Write(Utf8JsonWriter writer, ErrorDetails value, JsonSerializerOptions options)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
