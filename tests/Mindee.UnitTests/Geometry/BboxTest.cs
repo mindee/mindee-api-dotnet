77d3fdfd0bb77ada3b68bc3c9a1400397a87adbb
Kvin <kevin.blenner@mindee.co>
2023-02-07 11:46:37 +0100

    :sparkles: support Math Bbox concept

---
+++ tests/Mindee.UnitTests/Geometry/BboxTest.cs
@@ -0,0 +1,41 @@
+﻿using Mindee.Geometry;
+
+namespace Mindee.UnitTests.Geometry
+{
+    [Trait("Category", "Geometry - Bbox")]
+    public class BboxTest
+    {
+        [Fact]
+        public void CreateFromOnePolygon()
+        {
+            var polygon = new Polygon(new List<Point>()
+            {
+                new Point(0.081, 0.442),
+                new Point(0.15, 0.442),
+                new Point(0.15, 0.451),
+                new Point(0.081, 0.451)
+            });
+
+            Bbox bbox = BboxCreation.Create(polygon);
+
+            Assert.Equal(0.442, bbox.MinY);
+            Assert.Equal(0.081, bbox.MinX);
+            Assert.Equal(0.451, bbox.MaxY);
+            Assert.Equal(0.15, bbox.MaxX);
+        }
+
+        [Fact]
+        public void Merge2Bbox()
+        {
+            var firstBbox = new Bbox(0.081, 0.15, 0.442, 0.451);
+            var secondBbox = new Bbox(0.157, 0.26, 0.442, 0.451);
+
+            firstBbox.Merge(secondBbox);
+
+            Assert.Equal(0.442, firstBbox.MinY);
+            Assert.Equal(0.081, firstBbox.MinX);
+            Assert.Equal(0.451, firstBbox.MaxY);
+            Assert.Equal(0.26, firstBbox.MaxX);
+        }
+    }
+}
---
+++ tests/Mindee.UnitTests/Geometry/FakesPolygon.cs
@@ -0,0 +1,39 @@
+﻿using Mindee.Geometry;
+
+namespace Mindee.UnitTests.Geometry
+{
+    public static class FakesPolygon
+    {
+        public static Polygon GetPolygonAsRectangle()
+        {
+            return new Polygon(GetPointsAsRectangle());
+        }
+
+        public static Polygon GetPolygonWichIsNotRectangle()
+        {
+            return new Polygon(GetPointsWichIsNotRectangle());
+        }
+
+        private static List<Point> GetPointsAsRectangle()
+        {
+            return new List<Point>()
+            {
+                new Point(0.123, 0.53),
+                new Point(0.175, 0.53),
+                new Point(0.175, 0.546),
+                new Point(0.123, 0.546)
+            };
+        }
+
+        private static List<Point> GetPointsWichIsNotRectangle()
+        {
+            return new List<Point>()
+            {
+                new Point(0.205, 0.407),
+                new Point(0.379, 0.407),
+                new Point(0.381, 0.43),
+                new Point(0.207, 0.43)
+            };
+        }
+    }
+}
