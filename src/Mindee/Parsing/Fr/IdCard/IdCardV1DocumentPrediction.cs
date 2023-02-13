65fa89f7828f3703aa04c8d009ac3c3d96ee4c84
Ianaré Sévi <ianare@mindee.co>
2023-02-13 19:24:01 +0100

    :boom: :bulb: Update French ID card and Carte Vitale using auto-generated code

--- src/Mindee/Parsing/Fr/IdCard/IdCardV1DocumentPrediction.cs
+++ src/Mindee/Parsing/Fr/IdCard/IdCardV1DocumentPrediction.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -7,86 +7,88 @@ using Mindee.Parsing.Common;
 namespace Mindee.Parsing.Fr.IdCard
 {
     /// <summary>
-    /// The french id card model for the v1.
+    /// Document data for Carte Nationale d'Identité, version 1.
     /// </summary>
     public class IdCardV1DocumentPrediction
     {
         /// <summary>
-        /// The authority which has issued the card.
-        /// </summary>
-        [JsonPropertyName("authority")]
-        public StringField Authority { get; set; }
-
-        /// <summary>
-        /// The birth date of the person.
-        /// </summary>
-        [JsonPropertyName("birth_date")]
-        public StringField BirthDate { get; set; }
-
-        /// <summary>
-        /// The birth place of the person.
-        /// </summary>
-        [JsonPropertyName("birth_place")]
-        public StringField BirthPlace { get; set; }
-
-        /// <summary>
-        /// The expiry date of the card.
-        /// </summary>
-        [JsonPropertyName("expiry_date")]
-        public StringField ExpiryDate { get; set; }
-
-        /// <summary>
-        /// The gender of the person.
-        /// </summary>
-        [JsonPropertyName("gender")]
-        public StringField Gender { get; set; }
-
-        /// <summary>
-        /// The list of the names of the person.
-        /// </summary>
-        [JsonPropertyName("given_names")]
-        public List<StringField> GivenNames { get; set; }
-
-        /// <summary>
-        /// The id number of the card.
+        /// The identification card number.
         /// </summary>
         [JsonPropertyName("id_number")]
         public StringField IdNumber { get; set; }
 
         /// <summary>
-        /// The first mrz value.
+        /// The given name(s) of the card holder.
         /// </summary>
-        [JsonPropertyName("mrz1")]
-        public StringField Mrz1 { get; set; }
+        [JsonPropertyName("given_names")]
+        public List<StringField> GivenNames { get; set; }
 
         /// <summary>
-        /// The second mrz value.
-        /// </summary>
-        [JsonPropertyName("mrz2")]
-        public StringField Mrz2 { get; set; }
-
-        /// <summary>
-        /// The surname of the person.
+        /// The surname of the card holder.
         /// </summary>
         [JsonPropertyName("surname")]
         public StringField Surname { get; set; }
 
         /// <summary>
-        /// A prettier reprensentation of the current model values.
+        /// The date of birth of the card holder.
+        /// </summary>
+        [JsonPropertyName("birth_date")]
+        public DateField BirthDate { get; set; }
+
+        /// <summary>
+        /// The place of birth of the card holder.
+        /// </summary>
+        [JsonPropertyName("birth_place")]
+        public StringField BirthPlace { get; set; }
+
+        /// <summary>
+        /// The expiry date of the identification card.
+        /// </summary>
+        [JsonPropertyName("expiry_date")]
+        public DateField ExpiryDate { get; set; }
+
+        /// <summary>
+        /// The name of the issuing authority.
+        /// </summary>
+        [JsonPropertyName("authority")]
+        public StringField Authority { get; set; }
+
+        /// <summary>
+        /// The gender of the card holder.
+        /// </summary>
+        [JsonPropertyName("gender")]
+        public StringField Gender { get; set; }
+
+        /// <summary>
+        /// Machine Readable Zone, first line
+        /// </summary>
+        [JsonPropertyName("mrz1")]
+        public StringField Mrz1 { get; set; }
+
+        /// <summary>
+        /// Machine Readable Zone, second line
+        /// </summary>
+        [JsonPropertyName("mrz2")]
+        public StringField Mrz2 { get; set; }
+
+        /// <summary>
+        /// A prettier representation of the current model values.
         /// </summary>
         public override string ToString()
         {
+            string givenNames = string.Join(", ", GivenNames.Select(item => item));
+
             StringBuilder result = new StringBuilder();
-            result.Append($":Authority: {Authority}\n");
-            result.Append($":Given names: {string.Join(" ", GivenNames.Select(gn => gn))}\n");
+            result.Append($":Identity Number: {IdNumber}\n");
+            result.Append($":Given Name(s): {givenNames}\n");
             result.Append($":Surname: {Surname}\n");
+            result.Append($":Date of Birth: {BirthDate}\n");
+            result.Append($":Place of Birth: {BirthPlace}\n");
+            result.Append($":Expiry Date: {ExpiryDate}\n");
+            result.Append($":Issuing Authority: {Authority}\n");
             result.Append($":Gender: {Gender}\n");
-            result.Append($":ID Number: {IdNumber}\n");
-            result.Append($":Birth date: {BirthDate}\n");
-            result.Append($":Birth place: {BirthPlace}\n");
-            result.Append($":Expiry date: {ExpiryDate}\n");
-            result.Append($":MRZ 1: {Mrz1}\n");
-            result.Append($":MRZ 2: {Mrz2}\n");
+            result.Append($":MRZ Line 1: {Mrz1}\n");
+            result.Append($":MRZ Line 2: {Mrz2}\n");
 
             return SummaryHelper.Clean(result.ToString());
         }
