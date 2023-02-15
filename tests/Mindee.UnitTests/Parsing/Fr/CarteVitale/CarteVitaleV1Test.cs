023173fc9ed23f242421a2d162ed323ff731dc0a
ianaré <97107275+ianardee@users.noreply.github.com>
2023-02-15 16:00:42 +0100

    :white_check_mark: add auto-generated unit tests (#121)

--- tests/Mindee.UnitTests/Parsing/Fr/CarteVitale/CarteVitaleV1Test.cs
+++ tests/Mindee.UnitTests/Parsing/Fr/CarteVitale/CarteVitaleV1Test.cs
@@ -1,27 +1,33 @@
 using Mindee.Parsing;
+using Mindee.Parsing.Common;
 using Mindee.Parsing.Fr.CarteVitale;
 
 namespace Mindee.UnitTests.Parsing.Fr.CarteVitale
 {
-    [Trait("Category", "FR Carte Vitale V1")]
+    [Trait("Category", "CarteVitaleV1")]
     public class CarteVitaleV1Test
     {
         [Fact]
         public async Task Predict_CheckSummary()
         {
-            var mindeeAPi = GetMindeeApiForCarteVitale();
-            var prediction = await mindeeAPi.PredictAsync<CarteVitaleV1Inference>(ParsingTestBase.GetFakePredictParameter());
-
+            var prediction = await GetPrediction();
             var expected = File.ReadAllText("Resources/fr/carte_vitale/response_v1/summary_full.rst");
-
-            Assert.Equal(
-                expected,
-                prediction.ToString());
+            Assert.Equal(expected, prediction.ToString());
         }
 
-        private MindeeApi GetMindeeApiForCarteVitale(string fileName = "Resources/fr/carte_vitale/response_v1/complete.json")
+        [Fact]
+        public async Task Predict_CheckPage0()
         {
-            return ParsingTestBase.GetMindeeApi(fileName);
+            var prediction = await GetPrediction();
+            var expected = File.ReadAllText("Resources/fr/carte_vitale/response_v1/summary_page0.rst");
+            Assert.Equal(expected, prediction.Inference.Pages[0].ToString());
+        }
+
+        private async Task<Document<CarteVitaleV1Inference>> GetPrediction()
+        {
+            string fileName = "Resources/fr/carte_vitale/response_v1/complete.json";
+            var mindeeAPi = ParsingTestBase.GetMindeeApi(fileName);
+            return await mindeeAPi.PredictAsync<CarteVitaleV1Inference>(ParsingTestBase.GetFakePredictParameter());
         }
     }
 }
