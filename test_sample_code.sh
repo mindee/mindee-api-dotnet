cd3ac8a94cd2bea5e1d407f365d9ccfdccdd1d5e
Kvin <kevin.blenner@mindee.co>
2023-02-17 15:57:32 +0100

    debug

--- test_sample_code.sh
+++ test_sample_code.sh
@@ -3,6 +3,9 @@
 for f in `find docs/code_samples -name "*.txt"`
 do
   echo $f
+  ls -ali
+  ls tests/Resources/pdf
+  ls tests/resources/pdf
   cat docs/code_samples/base.csx $f > _test.csx
   sed -i 's/my-api-key/$1/' _test.csx
   sed -i 's/\/path\/to\/the\/file.ext/tests\/Ressources\/pdf\/blank_1.pdf/' _test.csx
