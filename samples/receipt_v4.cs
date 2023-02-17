6aa0fd98b547c8a756ab04c0ccdc26879486d06b
Kvin <kevin.blenner@mindee.co>
2023-02-17 09:37:58 +0100

    wording

--- samples/receipt_v4.cs
+++ samples/receipt_v4.cs
@@ -1,8 +1,8 @@
 using Mindee;
 using Mindee.Parsing.Receipt;
 
-string apiKey = "MyApiKey";
-string filePath = "MyFilePath";
+string apiKey = "my-api-key";
+string filePath = "/path/to/the/file.ext";
 
 MindeeClient mindeeClient = MindeeClientInit.Create(apiKey);
 
@@ -10,4 +10,4 @@ var documentParsed = await mindeeClient
     .LoadDocument(File.OpenRead(filePath), System.IO.Path.GetFileName(filePath))
     .ParseAsync<ReceiptV4Inference>();
 
-System.Console.WriteLine(documentParsed.Inference.DocumentPrediction.ToString());
+System.Console.WriteLine(documentParsed.ToString());
