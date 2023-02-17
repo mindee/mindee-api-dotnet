16302b804810378d5230b0c1403b090fd3a69f4d
Kvin <kevin.blenner@mindee.co>
2023-02-17 17:45:04 +0100

    test

--- test_sample_code.sh
+++ test_sample_code.sh
@@ -1,13 +1,12 @@
 #! /bin/sh
-
 for f in `find docs/code_samples -name "*.txt"`
 do
   echo $f
   ls -ali
   ls tests/resources/pdf
   cat docs/code_samples/base.csx $f > _test.csx
-  sed -i 's/my-api-key/$1/' _test.csx
-  sed -i 's/\/path\/to\/the\/file.ext/tests\/ressources\/pdf\/blank_1.pdf/' _test.csx
+  sed -i `s/my-api-key/$1/` _test.csx
+  sed -i "s/\/path\/to\/the\/file.ext/tests\/ressources\/pdf\/blank_1.pdf/" _test.csx
   cat _test.csx
   dotnet-script _test.csx
 done
