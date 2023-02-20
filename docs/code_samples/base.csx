53df9612ae88c7c1d39e98d1f75f6febba587fbd
Kevin <110382240+Kvindee@users.noreply.github.com>
2023-02-20 10:38:47 +0100

    :white_check_mark: add sample codes tests (#123)

---
+++ docs/code_samples/base.csx
@@ -0,0 +1,2 @@
+#!/usr/bin/env dotnet-script
+#r "nuget: Mindee, 2.0.0-test"
---
+++ test_sample_code.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+set -e
+for f in `find docs/code_samples -name "*.txt"`
+do
+  echo $f
+  cat docs/code_samples/base.csx $f > _test.csx
+  sed -i "s/my-api-key/$1/" _test.csx
+  sed -i "s/\/path\/to\/the\/file.ext/tests\/resources\/pdf\/blank_1.pdf/" _test.csx
+  dotnet-script _test.csx
+done
