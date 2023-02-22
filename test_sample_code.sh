a245b706489246022a174c975ab2cd713f11743f
Kvin <kevin.blenner@mindee.co>
2023-02-22 10:19:59 +0100

    :construction_worker: add custom and default code samples

--- test_sample_code.sh
+++ test_sample_code.sh
@@ -2,9 +2,14 @@
 set -e
 for f in `find docs/code_samples -name "*.txt"`
 do
-  echo $f
-  cat docs/code_samples/base.csx $f > _test.csx
-  sed -i "s/my-api-key/$1/" _test.csx
-  sed -i "s/\/path\/to\/the\/file.ext/tests\/resources\/pdf\/blank_1.pdf/" _test.csx
-  dotnet-script _test.csx
+  if echo "$f" | grep -q "custom_1.txt" 
+  then 
+    continue
+  else
+    echo $f
+    cat docs/code_samples/base.csx $f > _test.csx
+    sed -i "s/my-api-key/$1/" _test.csx
+    sed -i "s/\/path\/to\/the\/file.ext/tests\/resources\/pdf\/blank_1.pdf/" _test.csx
+    # dotnet-script _test.csx
+  fi
 done
