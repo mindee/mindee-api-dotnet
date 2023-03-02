db86c52cd160c64bb6630d2db16bd4209353043d
Kevin <110382240+Kvindee@users.noreply.github.com>
2023-03-02 17:02:58 +0100

    :sparkles: support invoice splitter v1 (#135)

---
+++ src/Mindee/Parsing/InvoiceSplitter/InvoiceSplitterV1Inference.cs
@@ -0,0 +1,12 @@
+using Mindee.Parsing.Common;
+
+namespace Mindee.Parsing.InvoiceSplitter
+{
+    /// <summary>
+    /// The invoice splitter v1 definition.
+    /// </summary>
+    [Endpoint("invoice_splitter_beta", "1")]
+    public class InvoiceSplitterV1Inference : Inference<InvoiceSplitterV1DocumentPrediction, InvoiceSplitterV1DocumentPrediction>
+    {
+    }
+}
