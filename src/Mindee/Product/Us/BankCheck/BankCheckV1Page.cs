6079f939628af9684c26c1e386cdbe05a4b002be
Ianaré Sévi <ianare@mindee.co>
2023-07-21 01:01:56 +0200

    :bug: fix page output in us bank check

---
+++ src/Mindee/Product/Us/BankCheck/BankCheckV1Page.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using Mindee.Parsing;
+using Mindee.Parsing.Standard;
+
+namespace Mindee.Product.Us.BankCheck
+{
+    /// <summary>
+    /// Page data for Bank Checks (beta), API version 1.
+    /// </summary>
+    public sealed class BankCheckV1Page : BankCheckV1Document
+    {
+        /// <summary>
+        /// The position of the check on the document.
+        /// </summary>
+        [JsonPropertyName("check_position")]
+        public PositionField CheckPosition { get; set; }
+
+        /// <summary>
+        /// List of signature positions
+        /// </summary>
+        [JsonPropertyName("signatures_positions")]
+        public IList<PositionField> SignaturesPositions { get; set; } = new List<PositionField>();
+
+        /// <summary>
+        /// A prettier representation of the current model values.
+        /// </summary>
+        public override string ToString()
+        {
+            string signaturesPositions = string.Join(
+                "\n " + string.Concat(Enumerable.Repeat(" ", 22)),
+                SignaturesPositions.Select(item => item));
+            StringBuilder result = new StringBuilder();
+            result.Append($":Check Position: {CheckPosition}\n");
+            result.Append($":Signature Positions: {signaturesPositions}\n");
+            result.Append(base.ToString());
+
+            return SummaryHelper.Clean(result.ToString());
+        }
+    }
+}
