140661869d31f06f009bdb5d26d0973627a0a11f
Ianaré Sévi <ianare@mindee.co>
2023-07-21 13:06:01 +0200

    :bulb: better description of inference classes

--- src/Mindee/Product/Fr/CarteVitale/CarteVitaleV1.cs
+++ src/Mindee/Product/Fr/CarteVitale/CarteVitaleV1.cs
@@ -4,7 +4,7 @@ using Mindee.Parsing.Common;
 namespace Mindee.Product.Fr.CarteVitale
 {
     /// <summary>
-    /// The definition for carte_vitale v1.
+    /// The definition for Carte Vitale, API version 1.
     /// </summary>
     [Endpoint("carte_vitale", "1")]
     public sealed class CarteVitaleV1 : Inference<CarteVitaleV1Document, CarteVitaleV1Document>
--- src/Mindee/Product/Passport/PassportV1.cs
+++ src/Mindee/Product/Passport/PassportV1.cs
@@ -4,7 +4,7 @@ using Mindee.Parsing.Common;
 namespace Mindee.Product.Passport
 {
     /// <summary>
-    /// The definition for passport v1.
+    /// The definition for Passport, API version 1.
     /// </summary>
     [Endpoint("passport", "1")]
     public sealed class PassportV1 : Inference<PassportV1Document, PassportV1Document>
