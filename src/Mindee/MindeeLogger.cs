0cf4a82b96ec4c85db6bffc0b55241f894338364
ianaré <97107275+ianardee@users.noreply.github.com>
2023-09-22 10:10:36 +0200

    :loud_sound: add global logging system (#187)

---
+++ src/Mindee/MindeeLogger.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Logging;
+
+namespace Mindee
+{
+    /// <summary>
+    /// Global Mindee logger.
+    /// </summary>
+    public class MindeeLogger
+    {
+        private static ILogger _instance;
+
+        /// <summary>
+        /// Assign a LoggerFactory.
+        /// </summary>
+        /// <param name="loggerFactory"><see cref="ILoggerFactory"/>></param>
+        public static void Assign(ILoggerFactory loggerFactory)
+        {
+            _instance = loggerFactory.CreateLogger("MindeeClient");
+            _instance.LogDebug("Logger initialized");
+        }
+
+        /// <summary>
+        /// Get the logger instance.
+        /// Will be null if a logger factory has not been <see cref="Assign"/>.
+        /// </summary>
+        public static ILogger GetLogger()
+        {
+            return _instance;
+        }
+    }
+}
