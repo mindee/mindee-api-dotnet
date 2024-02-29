73b11825b51f19cf1edae5f73bf3b38cf067e576
ianaré sévi <ianare@mindee.co>
2024-02-29 18:54:36 +0100

    :sparkles: add FR Carte Grise v1

---
+++ src/Mindee/Product/Fr/CarteGrise/CarteGriseV1.cs
@@ -0,0 +1,22 @@
+
+
+using System.Text.Json.Serialization;
+using Mindee.Http;
+using Mindee.Parsing.Common;
+
+namespace Mindee.Product.Fr.CarteGrise
+{
+    /// <summary>
+    /// The definition for Carte Grise, API version 1.
+    /// </summary>
+    [Endpoint("carte_grise", "1")]
+    public sealed class CarteGriseV1 : Inference<CarteGriseV1Document, CarteGriseV1Document>
+    {
+        /// <summary>
+        /// The pages and the associated values which were detected on the document.
+        /// </summary>
+        [JsonPropertyName("pages")]
+        [JsonConverter(typeof(PagesJsonConverter<CarteGriseV1Document>))]
+        public override Pages<CarteGriseV1Document> Pages { get; set; }
+    }
+}
