b288b8ff4d546e3075722d5c3e5f5549b50248be
ianaré <97107275+ianardee@users.noreply.github.com>
2024-04-05 12:11:18 +0200

    :bug: fix for writing custom classes to JSON (#216)

--- tests/Mindee.UnitTests/Geometry/PolygonTest.cs
+++ tests/Mindee.UnitTests/Geometry/PolygonTest.cs
@@ -14,5 +14,14 @@ namespace Mindee.UnitTests.Geometry
             Assert.Equal(expectedCentroid.X, actualCentroid.X);
             Assert.Equal(expectedCentroid.Y, actualCentroid.Y);
         }
+
+        [Fact]
+        public void Print_To_Json()
+        {
+            var polygon = FakesPolygon.GetPolygonAsRectangle();
+            Assert.Equal(
+                "[[0.123, 0.53], [0.175, 0.53], [0.175, 0.546], [0.123, 0.546]]",
+                polygon.ToJsonString());
+        }
     }
 }
