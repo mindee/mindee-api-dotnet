64ff01a7fcc6b53825fef03ee3bd525f5be834ec
ianaré sévi <ianare@mindee.co>
2024-05-27 12:10:43 +0200

    :sparkles: add support for US Mail

---
+++ src/Mindee/Parsing/Standard/BooleanField.cs
@@ -0,0 +1,41 @@
+using System.Text.Json.Serialization;
+using Mindee.Geometry;
+
+namespace Mindee.Parsing.Standard
+{
+    /// <summary>
+    /// Represent a string field.
+    /// </summary>
+    public class BooleanField : BaseField
+    {
+        /// <summary>
+        /// The value of the field.
+        /// </summary>
+        [JsonPropertyName("value")]
+        public bool? Value { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="value"><see cref="Value"/></param>
+        /// <param name="confidence"><see cref="BaseField.Confidence"/></param>
+        /// <param name="polygon"><see cref="BaseField.Polygon"/></param>
+        /// <param name="pageId"><see cref="BaseField.PageId"/></param>
+        public BooleanField(
+            bool? value,
+            double? confidence,
+            Polygon polygon,
+            int? pageId = null) : base(confidence, polygon, pageId)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Prettier representation.
+        /// </summary>
+        public override string ToString()
+        {
+            return SummaryHelper.FormatBool(Value);
+        }
+    }
+}
---
+++ src/Mindee/Product/Us/UsMail/UsMailV2SenderAddres.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using Mindee.Parsing;
+using Mindee.Parsing.Standard;
+
+namespace Mindee.Product.Us.UsMail
+{
+    /// <summary>
+    /// The address of the sender.
+    /// </summary>
+    public sealed class UsMailV2SenderAddres
+    {
+        /// <summary>
+        /// The city of the sender's address.
+        /// </summary>
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        /// <summary>
+        /// The complete address of the sender.
+        /// </summary>
+        [JsonPropertyName("complete")]
+        public string Complete { get; set; }
+
+        /// <summary>
+        /// The postal code of the sender's address.
+        /// </summary>
+        [JsonPropertyName("postal_code")]
+        public string PostalCode { get; set; }
+
+        /// <summary>
+        /// Second part of the ISO 3166-2 code, consisting of two letters indicating the US State.
+        /// </summary>
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        /// <summary>
+        /// The street of the sender's address.
+        /// </summary>
+        [JsonPropertyName("street")]
+        public string Street { get; set; }
+
+        /// <summary>
+        /// Output the object in a format suitable for inclusion in an rST field list.
+        /// </summary>
+        public string ToFieldList()
+        {
+            Dictionary<string, string> printable = PrintableValues();
+            return "\n"
+                + $"  :City: {printable["City"]}\n"
+                + $"  :Complete Address: {printable["Complete"]}\n"
+                + $"  :Postal Code: {printable["PostalCode"]}\n"
+                + $"  :State: {printable["State"]}\n"
+                + $"  :Street: {printable["Street"]}\n";
+        }
+
+        /// <summary>
+        /// A prettier representation of the line values.
+        /// </summary>
+        public override string ToString()
+        {
+            Dictionary<string, string> printable = PrintableValues();
+            return "City: "
+              + printable["City"]
+              + ", Complete Address: "
+              + printable["Complete"]
+              + ", Postal Code: "
+              + printable["PostalCode"]
+              + ", State: "
+              + printable["State"]
+              + ", Street: "
+              + printable["Street"].Trim();
+        }
+
+        private Dictionary<string, string> PrintableValues()
+        {
+            return new Dictionary<string, string>()
+            {
+                {"City", SummaryHelper.FormatString(City, 15)},
+                {"Complete", SummaryHelper.FormatString(Complete, 35)},
+                {"PostalCode", SummaryHelper.FormatString(PostalCode)},
+                {"State", SummaryHelper.FormatString(State)},
+                {"Street", SummaryHelper.FormatString(Street, 25)},
+            };
+        }
+    }
+}
