9e6f8cc4c70e22f91e04529967f441792712bae1
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-07-15 15:20:50 +0200

    use more correct syntax

--- tests/Mindee.IntegrationTests/TestingUtilities.cs
+++ tests/Mindee.IntegrationTests/TestingUtilities.cs
@@ -2,33 +2,33 @@ namespace Mindee.IntegrationTests
 {
     public class TestingUtilities
     {
-
         public static string GetVersion(string rstStr)
         {
-            int versionLineStartPos = rstStr.IndexOf(":Product: ");
-            int versionEndPos = rstStr.IndexOf("\n", versionLineStartPos);
+            int versionLineStartPos = rstStr.IndexOf(":Product: ", StringComparison.Ordinal);
+            int versionEndPos = rstStr.IndexOf("\n", versionLineStartPos, StringComparison.Ordinal);
 
             string substring = rstStr.Substring(versionLineStartPos, versionEndPos - versionLineStartPos);
-            int versionStartPos = substring.LastIndexOf(" v");
+            int versionStartPos = substring.LastIndexOf(" v", StringComparison.Ordinal);
 
             return substring.Substring(versionStartPos + 2);
         }
 
         public static string GetId(string rstStr)
         {
-            int idStartPos = rstStr.IndexOf(":Mindee ID: ") + 12;
-            int idEndPos = rstStr.IndexOf("\n:Filename:");
+            int idStartPos = rstStr.IndexOf(":Mindee ID: ", StringComparison.Ordinal) + 12;
+            int idEndPos = rstStr.IndexOf("\n:Filename:", StringComparison.Ordinal);
 
             return rstStr.Substring(idStartPos, idEndPos - idStartPos);
         }
 
         public static string GetFileName(string rstStr)
         {
-            int idStartPos = rstStr.IndexOf(":Filename: ") + 11;
-            int idEndPos = rstStr.IndexOf("\n\nInference");
+            int idStartPos = rstStr.IndexOf(":Filename: ", StringComparison.Ordinal) + 11;
+            int idEndPos = rstStr.IndexOf("\n\nInference", StringComparison.Ordinal);
 
             return rstStr.Substring(idStartPos, idEndPos - idStartPos);
         }
+
         // Note: Custom implementation for older versions of .NET
         public static async Task<byte[]> ReadAllBytesAsync(string path)
         {
@@ -36,14 +36,11 @@ namespace Mindee.IntegrationTests
                 throw new ArgumentNullException(nameof(path));
 
             byte[] buffer;
-            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
-            {
-                buffer = new byte[fs.Length];
-                await fs.ReadAsync(buffer, 0, (int)fs.Length);
-            }
+            using FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true);
+            buffer = new byte[fs.Length];
+            await fs.ReadAsync(buffer, 0, (int)fs.Length);
 
             return buffer;
         }
     }
 }
-
