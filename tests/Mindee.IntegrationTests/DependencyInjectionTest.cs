d7d59a55a3078c96d43924bb8e8bf87d9a2bbc14
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2024-07-23 12:31:42 +0200

    :bug: fix broken reference on worker service initialization (#246)

---
+++ tests/Mindee.IntegrationTests/DependencyInjectionTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Mindee.Extensions.DependencyInjection;
+using Mindee.Input;
+using Mindee.Product.Invoice;
+
+namespace Mindee.IntegrationTests
+{
+    [Trait("Category", "DI")]
+    public class DependencyInjectionTest
+    {
+        [Fact]
+        public async Task ShouldInstantiateWithDependencyInjection()
+        {
+            var builder = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMindeeClient();
+                });
+
+            var host = builder.Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var mindeeClient = services.GetRequiredService<MindeeClient>();
+                var inputSource = new LocalInputSource(new FileInfo("Resources/file_types/pdf/blank_1.pdf"));
+                var response = await mindeeClient.ParseAsync<InvoiceV4>(inputSource);
+                Assert.NotNull(response);
+            }
+
+
+            await host.StopAsync();
+        }
+    }
+}
