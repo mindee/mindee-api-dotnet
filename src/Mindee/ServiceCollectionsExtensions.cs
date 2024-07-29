eaba718dae648bd342bf668c22d0d80eeecacd52
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-07-29 19:17:34 +0200

    retrocompat for 472

--- src/Mindee/ServiceCollectionsExtensions.cs
+++ src/Mindee/ServiceCollectionsExtensions.cs
@@ -30,7 +30,7 @@ namespace Mindee.Extensions.DependencyInjection
 
 
             services.AddSingleton<MindeeApi>();
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13; // Safety for .NET 4.7.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12; // Safety for .NET 4.7.2
             RegisterRestSharpClient(services, throwOnError);
         }
 
