0d6260ea185a2f04562cfc4cf32600525f8ee340
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2024-09-26 10:52:20 +0200

    :arrow_up: update xunit dependency (#262)

--- example/AutoMultiReceiptsExtractionExample.cs
+++ example/AutoMultiReceiptsExtractionExample.cs
@@ -4,22 +4,28 @@ using Mindee.Input;
 using Mindee.Product.MultiReceiptsDetector;
 using Mindee.Product.Receipt;
 
-var apiKey = "my-api-key";
-var mindeeClient = new MindeeClient(apiKey);
-var myFilePath = "path/to/my/file.ext";
 
-var inputSource = new LocalInputSource(myFilePath);
-var resutlSplit = await mindeeClient.ParseAsync<MultiReceiptsDetectorV1>(inputSource);
-
-var imageExtractor = new ImageExtractor(inputSource);
-
-foreach (var page in resutlSplit.Document.Inference.Pages)
+async Task processReceipts(string filePath)
 {
-    var subImages = imageExtractor.ExtractImagesFromPage(page.Prediction.Receipts, page.Id);
-    foreach (var subImage in subImages)
+    var mindeeClient = new MindeeClient("my-api-key");
+    var inputSource = new LocalInputSource(filePath);
+    var resutlSplit = await mindeeClient.ParseAsync<MultiReceiptsDetectorV1>(inputSource);
+
+    var imageExtractor = new ImageExtractor(inputSource);
+
+    foreach (var page in resutlSplit.Document.Inference.Pages)
     {
-        // subImage.WriteToFile($"/path/to/my/extracted/file/folder"); // Optionally: write to a file
-        var resultReceipt = await mindeeClient.ParseAsync<ReceiptV5>(subImage.AsInputSource());
-        Console.WriteLine(resultReceipt.Document);
+        var subImages = imageExtractor.ExtractImagesFromPage(page.Prediction.Receipts, page.Id);
+        foreach (var subImage in subImages)
+        {
+            // subImage.WriteToFile($"/path/to/my/extracted/file/folder"); // Optionally: write to a file
+            var resultReceipt = await mindeeClient.ParseAsync<ReceiptV5>(subImage.AsInputSource());
+            Console.WriteLine(resultReceipt.Document);
+        }
     }
 }
+
+
+var myFilePath = "path/to/my/file.ext";
+
+await processReceipts(myFilePath);
