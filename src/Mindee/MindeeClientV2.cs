51ba0ec0ff9c995ffebbf8ff26ff52cb972af8d3
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-06-12 16:32:47 +0200

    add url input source polling for V2

--- src/Mindee/MindeeClientV2.cs
+++ src/Mindee/MindeeClientV2.cs
@@ -215,33 +215,72 @@ namespace Mindee
                 predictOptions,
                 pageOptions);
 
-            int maxRetries = pollingOptions.MaxRetries + 1;
+            return await PollForResultsAsync(enqueueResponse, pollingOptions);
+        }
 
+
+
+        /// <summary>
+        /// Add the document to an async queue, poll, and parse when complete.
+        /// </summary>
+        /// <param name="inputSource"><see cref="LocalInputSource"/></param>
+        /// <param name="modelId"><see cref="string"/></param>
+        /// <param name="predictOptions"><see cref="PageOptions"/></param>
+        /// <param name="pollingOptions"><see cref="AsyncPollingOptions"/></param>
+        /// <returns><see cref="AsyncPredictResponseV2"/></returns>
+        /// <exception cref="MindeeException"></exception>
+        public async Task<AsyncPredictResponseV2> EnqueueAndParseAsync(
+            UrlInputSource inputSource
+            , string modelId
+            , PredictOptions predictOptions = null
+            , AsyncPollingOptions pollingOptions = null)
+        {
+            _logger?.LogInformation("Asynchronous parsing ...");
+
+            if (pollingOptions == null)
+            {
+                pollingOptions = new AsyncPollingOptions();
+            }
+
+            var enqueueResponse = await EnqueueAsync(
+                inputSource,
+                modelId,
+                predictOptions);
+
+            return await PollForResultsAsync(enqueueResponse, pollingOptions);
+        }
+
+        /// <summary>
+        /// Poll for results until the prediction is retrieved or the max amount of attempts is reached.
+        /// </summary>
+        /// <param name="enqueueResponse"><see cref="AsyncPredictResponse{TInferenceModel}"/></param>
+        /// <param name="pollingOptions"><see cref="AsyncPollingOptions"/></param>
+        /// <returns><see cref="AsyncPredictResponse{TInferenceModel}"/></returns>
+        /// <exception cref="MindeeException">Thrown when maxRetries is reached and the result isn't ready.</exception>
+        private async Task<AsyncPredictResponseV2> PollForResultsAsync(
+            AsyncPredictResponseV2 enqueueResponse,
+            AsyncPollingOptions pollingOptions)
+        {
+            int maxRetries = pollingOptions.MaxRetries + 1;
             string jobId = enqueueResponse.Job.Id;
             _logger?.LogInformation("Enqueued with job ID: {}", jobId);
-
             _logger?.LogInformation(
                 "Waiting {} seconds before attempting to retrieve the document...",
                 pollingOptions.InitialDelaySec);
             Thread.Sleep(pollingOptions.InitialDelayMilliSec);
-
             int retryCount = 1;
             AsyncPredictResponseV2 response;
-
             while (retryCount < maxRetries)
             {
                 Thread.Sleep(pollingOptions.IntervalMilliSec);
                 _logger?.LogInformation("Attempting to retrieve: {} of {}", retryCount, maxRetries);
-
                 response = await ParseQueuedAsync(jobId);
                 if (response.Document != null)
                 {
                     return response;
                 }
-
                 retryCount++;
             }
-
             throw new MindeeException($"Could not complete after {retryCount} attempts.");
         }
 
