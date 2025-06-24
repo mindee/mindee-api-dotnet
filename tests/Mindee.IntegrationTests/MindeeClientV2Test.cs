cb2f9b329e18f144ff0e30baf86aceda5bc49f20
Ianaré Sévi <ianare@mindee.co>
2025-06-24 16:25:47 +0200

    add envvars

--- tests/Mindee.IntegrationTests/MindeeClientV2Test.cs
+++ tests/Mindee.IntegrationTests/MindeeClientV2Test.cs
@@ -10,18 +10,20 @@ namespace Mindee.IntegrationTests
     public class MindeeClientV2Test
     {
         private readonly MindeeClientV2 _mindeeClientV2;
+        private readonly string? _findocModelId;
 
         public MindeeClientV2Test()
         {
             var apiKey = Environment.GetEnvironmentVariable("MindeeV2__ApiKey");
             _mindeeClientV2 = TestingUtilities.GetOrGenerateMindeeClientV2(apiKey);
+            _findocModelId = Environment.GetEnvironmentVariable("MindeeV2__Findoc__Model__Id");
         }
 
         [Fact]
         public async Task Parse_File_Standard_MultiplePages_MustSucceed()
         {
             var inputSource = new LocalInputSource("Resources/file_types/pdf/multipage_cut-2.pdf");
-            var predictOptions = new PredictOptionsV2(Environment.GetEnvironmentVariable("Model__Id"));
+            var predictOptions = new PredictOptionsV2(modelId: _findocModelId);
             var response = await _mindeeClientV2.EnqueueAndParseAsync(inputSource, predictOptions);
             Assert.NotNull(response);
             Assert.NotNull(response.Inference);
@@ -36,7 +38,7 @@ namespace Mindee.IntegrationTests
             var inputSource =
                 new UrlInputSource(
                     "https://raw.githubusercontent.com/mindee/client-lib-test-data/main/products/expense_receipts/default_sample.jpg");
-            var predictOptions = new PredictOptionsV2(Environment.GetEnvironmentVariable("Model__Id"));
+            var predictOptions = new PredictOptionsV2(modelId: _findocModelId);
             var response = await _mindeeClientV2.EnqueueAndParseAsync(inputSource, predictOptions);
             Assert.NotNull(response);
             Assert.NotNull(response.Inference);
