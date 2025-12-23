f90db41e1db1b34e734dff04632ad34216498f7c
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-12-23 11:36:59 +0100

    fix headers for tests?

--- src/Mindee/Http/MindeeApiV2.cs
+++ src/Mindee/Http/MindeeApiV2.cs
@@ -31,7 +31,7 @@ namespace Mindee.Http
 
             var defaultHeaders = new Dictionary<string, string>
             {
-                { "Authorization", $"{mindeeSettings.Value.ApiKey}" }, { "Connection", "close" }
+                { "Authorization", $"{mindeeSettings.Value.ApiKey}" }
             };
             _httpClient.AddDefaultHeaders(defaultHeaders);
         }
