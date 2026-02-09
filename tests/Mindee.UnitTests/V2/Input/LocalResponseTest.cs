438fc0df457c6cb98ddda2bd8c6cbbdd2e6e2850
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-09 18:25:26 +0100

    :sparkles: add missing accessors & fix broken  accessor

--- tests/Mindee.UnitTests/V2/Input/LocalResponseTest.cs
+++ tests/Mindee.UnitTests/V2/Input/LocalResponseTest.cs
@@ -8,7 +8,7 @@ namespace Mindee.UnitTests.V2.Input
     public class LocalResponseTest
     {
         // File which the signature applies to.
-        private const string FilePath = Constants.V2RootDir + "inference/standard_field_types.json";
+        private const string FilePath = Constants.V2RootDir + "products/extraction/standard_field_types.json";
 
         private static void AssertLocalResponse(LocalResponse localResponse)
         {
@@ -16,7 +16,7 @@ namespace Mindee.UnitTests.V2.Input
             const string secretKey = "ogNjY44MhvKPGTtVsI8zG82JqWQa68woYQH";
 
             // Real signature using fake secret key.
-            const string signature = "1df388c992d87897fe61dfc56c444c58fc3c7369c31e2b5fd20d867695e93e85";
+            const string signature = "e51bdf80f1a08ed44ee161100fc30a25cb35b4ede671b0a575dc9064a3f5dbf1";
             Assert.False(localResponse.IsValidHmacSignature(
                 secretKey, "invalid signature is invalid"));
             Assert.Equal(signature, localResponse.GetHmacSignature(secretKey));
