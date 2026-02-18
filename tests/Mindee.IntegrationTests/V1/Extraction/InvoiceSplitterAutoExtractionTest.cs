470ba135a231ade4526acd1de0ec1775525253dd
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-18 12:14:54 +0100

    add debug

--- tests/Mindee.IntegrationTests/V1/Extraction/InvoiceSplitterAutoExtractionTest.cs
+++ tests/Mindee.IntegrationTests/V1/Extraction/InvoiceSplitterAutoExtractionTest.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Mindee.Extraction;
 using Mindee.Input;
 using Mindee.Parsing.Common;
@@ -24,24 +25,35 @@ namespace Mindee.IntegrationTests.V1.Extraction
         [Fact(Timeout = 180000)]
         public async Task GivenAPdf_ShouldExtractInvoicesStrict_MustSucceed()
         {
+            var sw = Stopwatch.StartNew();
+            void Log(string message) => Console.WriteLine($"[InvoiceSplitterAutoExtractionTest +{sw.Elapsed}] {message}");
+
             var apiKey = Environment.GetEnvironmentVariable("Mindee__ApiKey");
             var client = TestingUtilities.GetOrGenerateMindeeClient(apiKey);
             var invoiceSplitterBytes =
                 File.ReadAllBytes(Constants.V1ProductDir + "invoice_splitter/default_sample.pdf");
             var invoiceSplitterInputSource = new LocalInputSource(invoiceSplitterBytes, "default_sample.pdf");
+            Log("EnqueueAndParseAsync<InvoiceSplitterV1> start");
             var response = await client.EnqueueAndParseAsync<InvoiceSplitterV1>(invoiceSplitterInputSource);
+            Log("EnqueueAndParseAsync<InvoiceSplitterV1> done");
             var inference = response.Document.Inference;
 
+            Log("PdfExtractor ctor start");
             var extractor = new PdfExtractor(invoiceSplitterInputSource);
+            Log("GetPageCount start");
             Assert.Equal(2, extractor.GetPageCount());
+            Log("ExtractInvoices start");
             List<ExtractedPdf> extractedPdfsStrict = extractor.ExtractInvoices(
                 inference.Prediction.InvoicePageGroups, false);
+            Log("ExtractInvoices done");
             Assert.Equal(2, extractedPdfsStrict.Count);
             Assert.Equal("default_sample_001-001.pdf", extractedPdfsStrict[0].Filename);
             Assert.Equal("default_sample_002-002.pdf", extractedPdfsStrict[1].Filename);
 
+            Log("ParseAsync<InvoiceV4> start");
             var invoice0 =
                 await client.ParseAsync<InvoiceV4>(extractedPdfsStrict[0].AsInputSource());
+            Log("ParseAsync<InvoiceV4> done");
 
             var testStringRstInvoice0 = PrepareInvoiceReturn(
                 Constants.V1ProductDir + "invoices/response_v4/summary_full_invoice_p1.rst", invoice0.Document);
