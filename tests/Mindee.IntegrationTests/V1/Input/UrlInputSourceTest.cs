8ddad71c0eee92b92347d077af48c5073b74e030
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-02-18 10:25:00 +0100

    fix blank URL

--- tests/Mindee.IntegrationTests/V1/Input/UrlInputSourceTest.cs
+++ tests/Mindee.IntegrationTests/V1/Input/UrlInputSourceTest.cs
@@ -11,10 +11,11 @@ namespace Mindee.IntegrationTests.V1.Input
         public async Task GivenARemoteFile_MustRetrieveResponse()
         {
             var apiKey = Environment.GetEnvironmentVariable("Mindee__ApiKey");
+            var blankUrl = Environment.GetEnvironmentVariable("Mindee__V2__Se__Tests__Blank__Pdf__Url");
             var client = TestingUtilities.GetOrGenerateMindeeClient(apiKey);
             var remoteInput =
                 new UrlInputSource(
-                    "https://github.com/mindee/client-lib-test-data/blob/main/v1/products/invoice_splitter/invoice_5p.pdf?raw=true");
+                    blankUrl);
             var localInput = await remoteInput.AsLocalInputSource();
             Assert.Equal("invoice_5p.pdf", localInput.Filename);
             var result = await client.ParseAsync<InvoiceV4>(localInput);
