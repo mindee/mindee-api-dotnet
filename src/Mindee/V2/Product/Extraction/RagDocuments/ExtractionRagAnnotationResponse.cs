0b2e216f39f22f0d080a224aef8c201ae3527ed6
Ianaré Sévi <ianare@mindee.co>
2026-08-10 19:54:19 +0200

    generic class

--- src/Mindee/V2/Product/Extraction/RagDocuments/ExtractionRagAnnotationResponse.cs
+++ src/Mindee/V2/Product/Extraction/RagDocuments/ExtractionRagAnnotationResponse.cs
@@ -7,32 +7,13 @@ namespace Mindee.V2.Product.Extraction.RagDocuments
     /// <summary>
     /// Response for a RAG document.
     /// </summary>
-    public class ExtractionRagAnnotationResponse : BaseResponse
+    public class ExtractionRagAnnotationResponse : BaseRagAnnotationResponse
     {
-        /// <summary>
-        /// Unique identifier of the RAG document.
-        /// </summary>
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
-
         /// <summary>
         /// Model identifier linked to the RAG document.
         /// </summary>
         [JsonPropertyName("model_id")]
         public string ModelId { get; set; }
-
-        /// <summary>
-        /// Original filename of the uploaded document.
-        /// </summary>
-        [JsonPropertyName("filename")]
-        public string Filename { get; set; }
-
-        /// <summary>
-        /// Date and time of the document creation.
-        /// </summary>
-        [JsonPropertyName("created_at")]
-        [JsonConverter(typeof(DateTimeJsonConverter))]
-        public DateTime CreatedAt { get; set; }
 
         /// <summary>
         /// Number of times this document was used in an inference.
@@ -46,12 +27,6 @@ namespace Mindee.V2.Product.Extraction.RagDocuments
         [JsonPropertyName("last_match_at")]
         [JsonConverter(typeof(DateTimeJsonConverter))]
         public DateTime? LastMatchAt { get; set; }
-
-        /// <summary>
-        /// Current status of the RAG document.
-        /// </summary>
-        [JsonPropertyName("status")]
-        public string Status { get; set; }
 
         /// <summary>
         /// Annotation metadata associated with the document.
